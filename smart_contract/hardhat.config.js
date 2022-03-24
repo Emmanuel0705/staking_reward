b19510785176c5da3ed4210d6b37aebdea456a4e
Oluwatobi <emmanuelloluwatobi@gmail.com>
2022-03-24 19:47:18 +0100

    initial commit

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,5 +1,4 @@
 require("@nomiclabs/hardhat-waffle");
-require("@openzeppelin/hardhat-upgrades");
 // https://eth-rinkeby.alchemyapi.io/v2/38eGg1C5cY2KuAe3OJeBBWfPTuGmIlzS
 // https://eth-ropsten.alchemyapi.io/v2/keb1ih0mqSFkM_zKCrDaK9eri4s73hoN
 // f4aa392b3b595e5a7556807cbbe1c0fcc88dd772f17178413e5a1996e2edd142
